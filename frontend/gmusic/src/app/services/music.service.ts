3ee363317e46b9035b7909f08c8be1c4010b1912
João Carlos Pontes <jcarlospontesf@gmail.com>
2023-06-18 02:17:13 -0300

    Update music.service.ts

--- frontend/gmusic/src/app/services/music.service.ts
+++ frontend/gmusic/src/app/services/music.service.ts
@@ -14,7 +14,7 @@ export class MusicService {
 
   public indexmusica:number = 0;
 
-  private url:string = 'http://localhost:8080';
+  private url:string = 'http://4.246.219.153:8080';
 
   public selecionando:boolean = false;
 
